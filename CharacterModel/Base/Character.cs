7a123860a781cb6a41faf63080e7f8c90b75c73c
MaksimKutvitzky <baznalt@gmail.com>
2022-07-15 19:46:20 +0300

    HW-2 Adding base class and logger

---
+++ CharacterModel/Base/Character.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CharacterModel.Util;
+
+namespace CharacterModel
+{
+    public abstract class Character
+    {
+        protected static Random random = new Random();
+        protected virtual int HealthLimit
+        {
+            get => 100;
+        }
+        public string Name { get; }
+        private int _health;
+        private bool _isDead = false;
+        public bool IsDead { get => _isDead; private set => _isDead = value; }
+        protected abstract event Action<string> characterActionEvent;
+        private event Action<string> innerCharacterEvent;
+        protected abstract int Damage { get; }
+        public int Health
+        {
+            get => _health;
+
+            protected set
+            {
+                if (value >= HealthLimit)
+                {
+                    _health = HealthLimit;
+                }
+                else if (value <= 0)
+                {
+                    _isDead = true;
+                    _health = 0;
+                    innerCharacterEvent.Invoke($"{Name} is dead!");
+                }
+                else
+                {
+                    _health = value;
+                }
+            }
+        }
+
+        protected Character()
+        {
+        }
+
+        protected Character(int health, string name)
+        {
+            Health = health;
+            Name = name;
+            characterActionEvent += Logger.LogMessage;
+            innerCharacterEvent = Logger.LogMessage;
+        }
+        public abstract void TakeDamage(int damage);
+
+
+        public abstract void Attack(Character character);
+    }
+}
---
+++ CharacterModel/Util/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterModel.Util
+{
+    public static class Logger
+    {
+        public static void LogMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
